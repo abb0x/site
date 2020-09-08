41776639268be71674de5a4bda8ec10b432d0a8f
abb0x <60525276+abb0x@users.noreply.github.com>
2020-09-08 10:51:15 +0300

    test site

---
+++ scriptic.js
@@ -0,0 +1,14 @@
+$(function() {
+    $('.multiple-items').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        // dots:true
+        });
+});
+
+// $('.multiple-items').slick({
+//     infinite: true,
+//     slidesToShow: 3,
+//     slidesToScroll: 3
+//   });
